bf7704ac726b24669c20f3e3be5f0c89a6db4278
Your Name <you@example.com>
2020-08-16 13:26:17 +0300

    First commit

---
+++ Assets/Scripts/CopyPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [SerializeField]
+    Transform transTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transTarget.position;
+    }
+}
---
+++ Assets/Scripts/RestartLevelWithCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevelWithCollision : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision colision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
